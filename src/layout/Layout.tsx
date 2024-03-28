309d3cd37098fb6ca92b615f4009435b36c75b45
Pusaka Manggala <pusakamanggala@gmail.com>
2024-03-28 22:51:28 +0700

    feat : layout

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="container flex flex-col items-center justify-center flex-1 py-5 space-y-5 ">
+        {children}
+      </main>
+    </div>
+  );
+}
