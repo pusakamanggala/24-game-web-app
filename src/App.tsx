2636fcbce19737de589b4b2678649458e7f44693
Pusaka Manggala <pusakamanggala@gmail.com>
2024-03-28 23:11:29 +0700

    feat : routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,44 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import Home from "./pages/Home";
+import Play from "./pages/Play";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./layout/Layout";
+import HowToPlay from "./pages/HowToPlay";
+import PageNotFound from "./pages/PageNotFound";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <Home />
+            </Layout>
+          }
+        />
+        <Route
+          path="/play/:cardNumber"
+          element={
+            <Layout>
+              <Play />
+            </Layout>
+          }
+        />
+        <Route
+          path="/how-to-play"
+          element={
+            <Layout>
+              <HowToPlay />
+            </Layout>
+          }
+        />
+
+        {/* 404 */}
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
