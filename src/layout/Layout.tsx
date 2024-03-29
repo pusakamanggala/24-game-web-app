765ab0cabfbed52baac3b097064852886c18e98f
Pusaka Manggala <pusakamanggala@gmail.com>
2024-03-29 13:25:39 +0700

    style : update button style and main tag

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,7 +2,7 @@ import Header from "../components/Header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-[100dvh]">
       <Header />
       <main className="container flex flex-col items-center justify-center flex-1 py-5 space-y-5 ">
         {children}
